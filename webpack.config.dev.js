0084fa6ae107ce62ed4086e6e3ddb42481d9ef21
Howseman <howsmayer@hotmail.com>
2019-09-06 14:49:25 -0500

    Add webpack-dev-server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,14 @@
-const common = require('./webpack.common');
+const path = require('path');
 const merge = require('webpack-merge');
+
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'none',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4000
+  }
 });
